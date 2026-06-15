69fb37286ea82188753f7731e657f56c946c83c0
phd <18435623100@163.com>
2026-06-15 11:07:23 +0800

    chore: 修改格式

--- tools/zsqk/sql/检查库存金额变动.sql
+++ tools/zsqk/sql/检查库存金额变动.sql
@@ -1,30 +1,31 @@
 -- 查找库存金额变动, 以便找到异常变动
+-- 📢 和上次库存凭证不一致的数据对比
 SELECT
-	wd.id,
-	wd.created_at,
-	wd.whse_prod_amount + wd.whse_item_amount - wd.account_amount -
-	lag(wd.whse_prod_amount + wd.whse_item_amount - wd.account_amount) 
-    OVER (ORDER BY id) as "本次变动值(库存-凭证)",
-	wd.whse_prod_amount - lag(wd.whse_prod_amount) 
-    OVER (ORDER BY id) as "标品变动值(库存)",
-	wd.whse_item_amount - lag(wd.whse_item_amount) 
-    OVER (ORDER BY id) as "非标品变动值(库存)",
-    wd.account_amount - lag(wd.account_amount) 
-    OVER (ORDER BY id) as "凭证变动值(凭证)"
+  wd.id,
+  wd.created_at,
+  wd.whse_prod_amount + wd.whse_item_amount - wd.account_amount -
+  lag(wd.whse_prod_amount + wd.whse_item_amount - wd.account_amount) 
+  OVER (ORDER BY id) as "本次变动值(库存-凭证)",
+  wd.whse_prod_amount - lag(wd.whse_prod_amount) 
+  OVER (ORDER BY id) as "标品变动值(库存)",
+  wd.whse_item_amount - lag(wd.whse_item_amount) 
+  OVER (ORDER BY id) as "非标品变动值(库存)",
+  wd.account_amount - lag(wd.account_amount) 
+  OVER (ORDER BY id) as "凭证变动值(凭证)"
 FROM
-	gy_warehouse_amount_diff AS wd
+  gy_warehouse_amount_diff AS wd
 ORDER BY
-	id DESC;
+  id DESC;
 
 -- 查找凭证中金额, 以便找到异常金额相符的凭证
 SELECT
-	j.voucher,j.account,sum(amount_cent)
+  j.voucher,j.account,sum(amount_cent)
 FROM
-	cw_voucher v,
-	cw_journal_entry j
+  cw_voucher v,
+  cw_journal_entry j
 WHERE
-	v.created_at >= 1760251500
-	AND v.created_at <= 1760255130
-	and v.id = j.voucher
-	group by j.voucher,j.account
-	;
+  v.created_at >= 1760251500
+  AND v.created_at <= 1760255130
+  and v.id = j.voucher
+  group by j.voucher,j.account
+  ;
